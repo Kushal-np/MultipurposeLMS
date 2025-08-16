4c50598f17d4fc6cc483dc64696bd632fd63b371
Kushal-np <wrckushal@gmail.com>
2025-08-16 10:35:49 +0545

    Written schema for users

---
+++ server/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    role:{
+        type:String,
+        enum:["user","instructor","admin"],
+        default:"user",
+    }
+},{
+    timestamps:true,
+})
+
+export default mongoose.model("user", userSchema)
