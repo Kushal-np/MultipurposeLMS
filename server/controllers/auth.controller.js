e2b37d3511835d57493249c271a8605b044276ec
Kushal-np <wrckushal@gmail.com>
2025-08-16 20:59:18 +0545

    finally this one is working

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,33 +1,38 @@
-import User from "../models/user.model.js"
-import bcrypt from 'bcryptjs'
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 import generateToken from "../utils/generateToken.js";
+export const register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
 
+    if (!name || !email || !password) {
+      return res.status(400).json({ success: false, message: "All fields are required" });
+    }
 
-export const register = async( req , res)=>{
-    try{
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ success: false, message: "Email already exists" });
+    }
 
-        res.send("Hello world")
-        const {name , email, password, role} = req.body;
-        const userExists = await User.findOne({email});
-        if(userExists){
-            return res.status(400).json({
-                message:"email already exists",
-            })
-        }
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const user = await User.create({name, email, password:hashedPassword , role})
+    const salt  = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+    const user = await User.create({name,email,password:hashedPassword , role})
 
-        const token = generateToken(user._id , user.role);
-        res.status(201).json({
-            _id: user._id,
-            name:user.name,
-            email:user.email,
-            role: user.role,
-            token // send token back 
-        })
-    }  
-    catch(error){
+    const token = generateToken(user._id , user.role )
 
-    }  
-}
+    res.cookie("jwt",token ,  {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    })
+    res.status(200).json({
+        success:true,
+        user
+    })
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import { connectDB } from './config/db.js';
 import authRoutes from './routes/auth.route.js'
 const app = express();
 app.use(express.json())
-app.use("api/v1/auth", authRoutes)
+app.use("/api/v1/auth", authRoutes)
 
-app.listen(process.env.PORT || 3000 , ()=>{
+app.listen(process.env.PORT || 4000 , ()=>{
     console.log(`Backend finally running on port`)
     connectDB();
 })
